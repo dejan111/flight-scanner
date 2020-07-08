c07bac96289cde2ab75f9c1dbc8cd54c0e332c63
dejan111 <dejan.pavkovic@gmail.com>
2020-07-08 22:36:34 +0200

    ADD Init commit

---
+++ flight-scanner-client/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { of } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  origin : string;
+  destination : string;
+
+  flightSearchForm = new FormGroup({
+    originInput: new FormControl(''),
+    destinationInput: new FormControl(''),
+    currencyInput: new FormControl(''),
+    dateRangeInput: new FormControl('')
+  });
+
+  currencies = this.getCurrencies();
+  currencyInputName = 'currencyInput'
+
+  title = 'Flight scanner';
+  minDate = this.getYesterday();
+
+  getCurrencies(){
+    return ['USD', 'EUR', 'HRK']
+  }
+  
+  getYesterday() {
+    let minDate = new Date();
+    minDate.setDate(minDate.getDate() - 1);
+
+    return minDate;
+  }
+
+  onSubmit(){
+    console.log(this.flightSearchForm.value);
+  };
+}
---
+++ flight-scanner-client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
